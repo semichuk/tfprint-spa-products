bacfcbbc21d1c44730cdd2672dd1b2c636b4b653
ki <70934055+semichuk@users.noreply.github.com>
2023-06-08 16:58:00 +0400

    Update connect_to_DB.php

--- rest_api/connect_to_DB.php
+++ rest_api/connect_to_DB.php
@@ -1,5 +1,5 @@
 <?php
 
 
-$host = "mysql:host=localhost;dbname=tfprint_mysql2020;charset=utf8";
-$pdo = new PDO($host, 'tfprint_mysql', 'SoyTXZlJ');
+$host = ""; //Здесь должен быть прописан хост
+$pdo = new PDO($host,); //А здесь пароль и логин БД
